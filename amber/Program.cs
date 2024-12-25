b5683cb5aad4faf05b253da63b6976f0ab2ecd4b
Lucas Phill <lucaspscp@outlook.com>
2024-12-25 01:26:43 -0300

    Exercicios sobre moedas e arrays

---
+++ amber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+Console.Clear();
+
+var valor = 10.75m;
+
+Console.WriteLine(valor.ToString("G", CultureInfo.CreateSpecificCulture("pt-BR"))); //generico
+Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR"))); //currency
+Console.WriteLine(valor.ToString("E04", CultureInfo.CreateSpecificCulture("pt-BR"))); //cientifica
+Console.WriteLine(valor.ToString("P", CultureInfo.CreateSpecificCulture("pt-BR"))); //porcentagem
+Console.WriteLine(valor.ToString("F", CultureInfo.CreateSpecificCulture("pt-BR"))); //precisão maior
+Console.WriteLine(valor.ToString("N", CultureInfo.CreateSpecificCulture("pt-BR"))); //sem formatação de moeda
+
+
+Console.WriteLine(Math.Round(valor)); //remove os decimais do numero / arredonda para o mais proximo
+Console.WriteLine(Math.Ceiling(valor)); //arredonda para cima
+Console.WriteLine(Math.Floor(valor)); //arredonda para baixo
+
+Console.WriteLine($"{valor:C}"); //outra forma de escrever a formatação
+Console.WriteLine($"{valor:E04}"); //outra forma de escrever a formatação
---
+++ sapphire/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+var meuArray = new int[5];
+int[] meuArray2 = new int[5] { 1, 2, 3, 4, 5 };
+
+meuArray[0] = 12;
+meuArray.Clone();
+
+Console.WriteLine(meuArray[0]);
+Console.WriteLine(meuArray.Length);
+
+// for (var item = 0; item < meuArray.Length; item++) { Console.WriteLine(meuArray[item]); }
+
+foreach (var item in meuArray) { Console.WriteLine(item); }
